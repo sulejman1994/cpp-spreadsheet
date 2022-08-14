72b047e050e7301d18655b35c85b81be5d6f1b16
Suleyman <100504478+sulejman1994@users.noreply.github.com>
2022-08-14 20:33:18 +0300

    Update sheet.h

--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -1,1 +1,40 @@
-/* разместите здесь свой код */
+#pragma once
+
+#include "cell.h"
+#include "common.h"
+
+#include <unordered_map>
+#include <functional>
+
+
+class Sheet : public SheetInterface {
+public:
+    Sheet();
+    ~Sheet();
+
+    void SetCell(Position pos, std::string text) override;
+     
+    const CellInterface* GetCell(Position pos) const override;
+    CellInterface* GetCell(Position pos) override;
+
+    void ClearCell(Position pos) override;
+      
+    Size GetPrintableSize() const override;
+
+    void PrintValues(std::ostream& output) const override;
+    void PrintTexts(std::ostream& output) const override;
+
+    static void ValidatePosition(Position pos);
+
+private:
+	mutable std::unordered_map<Position, Cell, Position::Hasher> table_;
+    DependencyGraph graph_;
+    mutable Size size_ = {0, 0}; 
+    
+    void SetEmptyNewReferencedCells(const std::vector<Position>& referenced_cells);
+    void RecalculateSize() const;
+
+};
+
+std::ostream& operator<<(std::ostream& out, const CellInterface::Value& value);
+
