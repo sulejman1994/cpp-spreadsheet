dd1d120c1100c1a9c1a8b6ea1dbcbd86fc302f28
Suleyman <100504478+sulejman1994@users.noreply.github.com>
2022-08-14 20:28:09 +0300

    Update FormulaAST.h

--- spreadsheet/FormulaAST.h
+++ spreadsheet/FormulaAST.h
@@ -6,43 +6,35 @@
 #include <forward_list>
 #include <functional>
 #include <stdexcept>
+#include <set>
 
 namespace ASTImpl {
 class Expr;
 }
-
+  
 class ParsingError : public std::runtime_error {
     using std::runtime_error::runtime_error;
 };
 
+
 class FormulaAST {
 public:
-    explicit FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr,
-                        std::forward_list<Position> cells);
+  
+    FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr, const std::set<Position>& cells = {});
+
     FormulaAST(FormulaAST&&) = default;
     FormulaAST& operator=(FormulaAST&&) = default;
     ~FormulaAST();
 
-    double Execute(/*добавьте нужные аргументы*/ args) const;
-    void PrintCells(std::ostream& out) const;
+    double Execute(const SheetInterface& sheet) const;
     void Print(std::ostream& out) const;
     void PrintFormula(std::ostream& out) const;
 
-    std::forward_list<Position>& GetCells() {
-        return cells_;
-    }
-
-    const std::forward_list<Position>& GetCells() const {
-        return cells_;
-    }
+    std::vector<Position> GetReferencedCells() const;
 
 private:
     std::unique_ptr<ASTImpl::Expr> root_expr_;
-
-    // physically stores cells so that they can be
-    // efficiently traversed without going through
-    // the whole AST
-    std::forward_list<Position> cells_;
+    std::vector<Position> cells_;
 };
 
 FormulaAST ParseFormulaAST(std::istream& in);
